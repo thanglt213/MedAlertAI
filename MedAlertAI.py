e9793208a7c2be0959f218e3433bff1077b2493d
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-10-07 14:59:52 +0700

    Update MedAlertAI.py

--- MedAlertAI.py
+++ MedAlertAI.py
@@ -14,6 +14,15 @@ train_file = st.file_uploader("Upload your training data CSV file", type=["csv"]
 # Upload file CSV cho dữ liệu dự đoán
 uploaded_file = st.file_uploader("Upload your input data CSV file", type=["csv"], key='data')
 
+# Nút để xóa file mô hình
+if st.button("Xóa file mô hình"):
+    model_file = 'model.pkl'
+    if os.path.exists(model_file):
+        os.remove(model_file)
+        st.success(f"File {model_file} đã được xóa.")
+    else:
+        st.warning(f"File {model_file} không tồn tại.")
+
 if train_file and uploaded_file:
     # Đọc dữ liệu huấn luyện
     train_data = pd.read_csv(train_file)
@@ -35,14 +44,13 @@ if train_file and uploaded_file:
     # Kiểm tra và xử lý giá trị NaN
     combined_data.fillna('missing', inplace=True)
 
-    # Chuyển đổi các trường thành kiểu dữ liệu mong muốn
+    # Chuyển tất cả các cột thành kiểu chuỗi
+    for column in combined_data.columns:
+        combined_data[column] = combined_data[column].astype(str)
+
+    # Chuyển đổi các trường cụ thể thành kiểu số
     numeric_columns = ['group', 'days_to_report', 'requested_amount_per_day']
     combined_data[numeric_columns] = combined_data[numeric_columns].apply(pd.to_numeric, errors='coerce')
-
-    # Chuyển đổi tất cả các cột còn lại thành kiểu object
-    for column in combined_data.columns:
-        if column not in numeric_columns:
-            combined_data[column] = combined_data[column].astype('object')
 
     # Xóa các cột có NaN sau khi chuyển đổi (nếu có)
     combined_data = combined_data.dropna()
@@ -92,14 +100,8 @@ if train_file and uploaded_file:
     result_df['Prediction'] = result_df['Prediction'].replace({1: 'Normal', -1: 'Anomaly'})
     st.write(result_df)
 
-    # Nút để xóa file mô hình
-    if st.button("Xóa file mô hình"):
-        if os.path.exists(model_file):
-            os.remove(model_file)
-            st.success(f"File {model_file} đã được xóa.")
-        else:
-            st.warning(f"File {model_file} không tồn tại.")
 else:
     st.warning("Vui lòng tải lên cả hai tệp dữ liệu huấn luyện và dữ liệu dự đoán.")
+
 
 
