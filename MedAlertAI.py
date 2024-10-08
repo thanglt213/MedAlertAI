34675ca061a5db341ccbd68a65ac4af96cdbd2bd
thanglt213 <161811573+thanglt213@users.noreply.github.com>
2024-10-08 07:45:41 +0700

    Update MedAlertAI.py

--- MedAlertAI.py
+++ MedAlertAI.py
@@ -61,7 +61,7 @@ if train_file and uploaded_file:
 
     # Chuyển đổi các trường cụ thể thành kiểu số
     numeric_columns = ['group', 'days_to_report', 'requested_amount_per_day']
-    combined_data[numeric_columns] = combined_data[numeric_columns].apply(pd.to_numeric, errors='coerce')
+    combined_data[numeric_columns] = combined_data[numeric_columns].astype(int64)
 
     # Xóa các cột có NaN sau khi chuyển đổi (nếu có)
     st.write("Shape của combined_data - dòng 67:", combined_data.shape)
